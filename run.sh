a260037ce950130abedee07ea6f3fea9c2496b21
yangnanbei <406631132@qq.com>
2025-02-09 18:54:20 +0800

    rust, part 1, add run.sh, yang_nanbei

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cargo b --release
+sudo setcap cap_net_admin=eip ./target/release/nb_trust
+./target/release/nb_trust &
+pid=$!
+sudo ip addr add 192.168.0.1/24 dev tun0
+sudo ip link set up dev tun0
+wait $pid
