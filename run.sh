32e94adf08f984e50f69ce198aca99508802eaa8
yangnanbei <406631132@qq.com>
2025-02-17 22:46:19 +0800

    rust, part 2, recognize and filter pkts, yang_nanbei

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ sudo setcap cap_net_admin=eip ./target/release/nb_trust
 pid=$!
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
+trap "kill $pid" INT TERM
 wait $pid
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 use std::io;
+
+const PROTO_IPV4: u16 = 0x0800;
+
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:x?}", nbytes, &buf[..nbytes]);
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        let flags = u16::from_be_bytes([buf[0], buf[1]]);
+        let proto = u16::from_be_bytes([buf[2], buf[3]]);
+        match proto != PROTO_IPV4 {
+            true => continue,
+            false => (),
+        }
+        eprintln!(
+            "read {} bytes, flags: {:x}, proto: {:x}: {:x?}",
+            nbytes - 4,
+            flags,
+            proto,
+            &buf[4..nbytes]
+        );
+    }
     Ok(())
 }
