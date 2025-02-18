5a564eb530c4367d3b49a75fcfa448243c3a72e7
yangnanbei <406631132@qq.com>
2025-02-18 23:10:16 +0800

    rust, part 3, parse ip and tcp header, yang_nanbei

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,55 @@
 use std::io;
 
 const PROTO_IPV4: u16 = 0x0800;
+const PROTO_TCP: u8 = 0x06;
+const ETH_HEADER_BYTES: usize = 4;
 
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
         let nbytes = nic.recv(&mut buf[..])?;
-        let flags = u16::from_be_bytes([buf[0], buf[1]]);
-        let proto = u16::from_be_bytes([buf[2], buf[3]]);
-        match proto != PROTO_IPV4 {
+        let _ethflags = u16::from_be_bytes([buf[0], buf[1]]);
+        let ethproto = u16::from_be_bytes([buf[2], buf[3]]);
+        match ethproto != PROTO_IPV4 {
             true => continue,
             false => (),
         }
-        eprintln!(
-            "read {} bytes, flags: {:x}, proto: {:x}: {:x?}",
-            nbytes - 4,
-            flags,
-            proto,
-            &buf[4..nbytes]
-        );
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&mut buf[ETH_HEADER_BYTES..nbytes]) {
+            Ok(p) => {
+                let src = p.source_addr();
+                let dst = p.destination_addr();
+                let proto = p.protocol();
+                let payload_len = p.payload_len();
+                if proto != PROTO_TCP {
+                    /* only handle tcp now */
+                    eprintln!("ignoreing packet proto {:?}", proto);
+                    continue;
+                }
+
+                match etherparse::TcpHeaderSlice::from_slice(&mut buf[ETH_HEADER_BYTES + p.slice().len()..nbytes]) {
+                    Ok(p) => {
+                        eprintln!(
+                            "{} -> {} {}b of tcp from port {} to {}",
+                            src,
+                            dst,
+                            payload_len,
+                            p.source_port(),
+                            p.destination_port(),
+                        );
+                    }
+                    Err(e) => {
+                        eprintln!("ignoreing weired packet {:?}", e);
+                        continue;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("ignoreing weired packet {:?}", e);
+                continue;
+            }
+        }
     }
     Ok(())
 }
