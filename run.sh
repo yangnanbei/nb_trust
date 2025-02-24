6b808d20f786a6475b130995bee4f67d510be797
yangnanbei <406631132@qq.com>
2025-02-24 21:55:55 +0800

    rust, part 3, parse ip and tcp header, yang_nanbei

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 cargo b --release
+ext=$?
+if [[ $ext -ne 0 ]]; then
+	exit $ext
+fi
+
 sudo setcap cap_net_admin=eip ./target/release/nb_trust
 ./target/release/nb_trust &
 pid=$!
+
 sudo ip addr add 192.168.0.1/24 dev tun0
 sudo ip link set up dev tun0
 trap "kill $pid" INT TERM
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
+use std::collections::HashMap;
 use std::io;
+use std::net::Ipv4Addr;
+
+mod tcp;
 
 const PROTO_IPV4: u16 = 0x0800;
 const PROTO_TCP: u8 = 0x06;
 const ETH_HEADER_BYTES: usize = 4;
 
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
+
 fn main() -> io::Result<()> {
+    let mut connections: HashMap<Quad, tcp::State> = Default::default();
     let nic = tun_tap::Iface::new("tun0", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
     loop {
@@ -16,28 +27,30 @@ fn main() -> io::Result<()> {
             false => (),
         }
 
-        match etherparse::Ipv4HeaderSlice::from_slice(&mut buf[ETH_HEADER_BYTES..nbytes]) {
-            Ok(p) => {
-                let src = p.source_addr();
-                let dst = p.destination_addr();
-                let proto = p.protocol();
-                let payload_len = p.payload_len();
+        match etherparse::Ipv4HeaderSlice::from_slice(&buf[ETH_HEADER_BYTES..nbytes]) {
+            Ok(iph) => {
+                let src = iph.source_addr();
+                let dst = iph.destination_addr();
+                let proto = iph.protocol();
                 if proto != PROTO_TCP {
                     /* only handle tcp now */
                     eprintln!("ignoreing packet proto {:?}", proto);
                     continue;
                 }
 
-                match etherparse::TcpHeaderSlice::from_slice(&mut buf[ETH_HEADER_BYTES + p.slice().len()..nbytes]) {
-                    Ok(p) => {
-                        eprintln!(
-                            "{} -> {} {}b of tcp from port {} to {}",
-                            src,
-                            dst,
-                            payload_len,
-                            p.source_port(),
-                            p.destination_port(),
-                        );
+                let ip_hdr_size = iph.slice().len();
+                match etherparse::TcpHeaderSlice::from_slice(
+                    &buf[ETH_HEADER_BYTES + iph.slice().len()..nbytes],
+                ) {
+                    Ok(tcph) => {
+                        let datai = ETH_HEADER_BYTES + ip_hdr_size + tcph.slice().len();
+                        connections
+                            .entry(Quad {
+                                src: (src, tcph.source_port()),
+                                dst: (dst, tcph.destination_port()),
+                            })
+                            .or_default()
+                            .on_packet(iph, tcph, &buf[datai..nbytes]);
                     }
                     Err(e) => {
                         eprintln!("ignoreing weired packet {:?}", e);
